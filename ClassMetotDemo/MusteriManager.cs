97eab8a7256532d2490c8fc12b5612f6ea525dd4
Ozan Ateşmen <atesmenozan@gmail.com>
2021-07-30 14:29:48 +0300

    odev

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri Basariyla Eklendi:" + musteri.Ad + " " + musteri.Soyadi );
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + "Id Numarali" +" "+ musteri.Ad +" "+ "Isimli Musteri Basariyla Listelendi");
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Yasi +" " + "Yasindaki Musteriler Basariyla Silindi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 01;
+            musteri1.Ad = "Enver";
+            musteri1.Soyadi = "Ecesoy";
+            musteri1.Yasi = 24;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 02;
+            musteri2.Ad = "Onur";
+            musteri2.Soyadi = "Yilmaz";
+            musteri2.Yasi = 23;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 03;
+            musteri3.Ad = "Ozan";
+            musteri3.Soyadi = "Atesmen";
+            musteri3.Yasi = 22;
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+            musteriManager.Ekle(musteri3);
+
+            musteriManager.Listele(musteri1);
+            musteriManager.Listele(musteri2);
+            musteriManager.Listele(musteri3);
+
+            musteriManager.Sil(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Sil(musteri3);
+        }
+    }
+}
